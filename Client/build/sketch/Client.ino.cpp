3784c0c6770ebb6586a521591e50a32b3e718eb7
NachoK00l <zeno77@outlook.be>
2026-03-13 15:59:27 +0100

    Made arduino sketches

---
+++ Client/build/sketch/Client.ino.cpp
@@ -0,0 +1,17 @@
+#line 1 "C:\\Users\\GaelHenriquezDevia\\OneDrive - Sint-Jozefinstituut Schoten\\Documenten\\Arduino\\Arduino-Wifi-Chatroom\\Client\\Client.ino"
+#include <Arduino.h>
+#include <WiFi.h>
+
+#line 4 "C:\\Users\\GaelHenriquezDevia\\OneDrive - Sint-Jozefinstituut Schoten\\Documenten\\Arduino\\Arduino-Wifi-Chatroom\\Client\\Client.ino"
+void setup();
+#line 8 "C:\\Users\\GaelHenriquezDevia\\OneDrive - Sint-Jozefinstituut Schoten\\Documenten\\Arduino\\Arduino-Wifi-Chatroom\\Client\\Client.ino"
+void loop();
+#line 4 "C:\\Users\\GaelHenriquezDevia\\OneDrive - Sint-Jozefinstituut Schoten\\Documenten\\Arduino\\Arduino-Wifi-Chatroom\\Client\\Client.ino"
+void setup()
+{
+}
+
+void loop()
+{
+}
+
---
+++ Server/build/sketch/Server.ino.cpp
@@ -0,0 +1,16 @@
+#line 1 "C:\\Users\\GaelHenriquezDevia\\OneDrive - Sint-Jozefinstituut Schoten\\Documenten\\Arduino\\Arduino-Wifi-Chatroom\\Server\\Server.ino"
+#include <Arduino.h>
+#include <WiFi.h>
+#line 3 "C:\\Users\\GaelHenriquezDevia\\OneDrive - Sint-Jozefinstituut Schoten\\Documenten\\Arduino\\Arduino-Wifi-Chatroom\\Server\\Server.ino"
+void setup();
+#line 7 "C:\\Users\\GaelHenriquezDevia\\OneDrive - Sint-Jozefinstituut Schoten\\Documenten\\Arduino\\Arduino-Wifi-Chatroom\\Server\\Server.ino"
+void loop();
+#line 3 "C:\\Users\\GaelHenriquezDevia\\OneDrive - Sint-Jozefinstituut Schoten\\Documenten\\Arduino\\Arduino-Wifi-Chatroom\\Server\\Server.ino"
+void setup()
+{
+}
+
+void loop()
+{
+}
+
