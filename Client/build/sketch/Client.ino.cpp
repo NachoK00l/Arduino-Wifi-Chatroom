ae628f4efae110843057557256e9c95bc2ed1445
NachoK00l <zeno77@outlook.be>
2026-03-13 19:35:09 +0100

    Created Client base code

--- Client/build/sketch/Client.ino.cpp
+++ Client/build/sketch/Client.ino.cpp
@@ -1,12 +1,12 @@
-#line 1 "C:\\Users\\GaelHenriquezDevia\\OneDrive - Sint-Jozefinstituut Schoten\\Documenten\\Arduino\\Arduino-Wifi-Chatroom\\Client\\Client.ino"
+#line 1 "C:\\Users\\zeno7\\Documents\\Arduino-Wifi-Chatroom\\Client\\Client.ino"
 #include <Arduino.h>
 #include <WiFi.h>
 
-#line 4 "C:\\Users\\GaelHenriquezDevia\\OneDrive - Sint-Jozefinstituut Schoten\\Documenten\\Arduino\\Arduino-Wifi-Chatroom\\Client\\Client.ino"
+#line 4 "C:\\Users\\zeno7\\Documents\\Arduino-Wifi-Chatroom\\Client\\Client.ino"
 void setup();
-#line 8 "C:\\Users\\GaelHenriquezDevia\\OneDrive - Sint-Jozefinstituut Schoten\\Documenten\\Arduino\\Arduino-Wifi-Chatroom\\Client\\Client.ino"
+#line 8 "C:\\Users\\zeno7\\Documents\\Arduino-Wifi-Chatroom\\Client\\Client.ino"
 void loop();
-#line 4 "C:\\Users\\GaelHenriquezDevia\\OneDrive - Sint-Jozefinstituut Schoten\\Documenten\\Arduino\\Arduino-Wifi-Chatroom\\Client\\Client.ino"
+#line 4 "C:\\Users\\zeno7\\Documents\\Arduino-Wifi-Chatroom\\Client\\Client.ino"
 void setup()
 {
 }
